606c434302df7ac6c9ee4dc3d9b535e6dbde58c9
defnot001 <marc.miller994@gmail.com>
2024-01-21 22:25:12 +0100

    implemented attachment notifications in game

--- src/main/kotlin/io/github/defnot001/discordbot/MessageReceivedListener.kt
+++ src/main/kotlin/io/github/defnot001/discordbot/MessageReceivedListener.kt
@@ -1,6 +1,7 @@
 package io.github.defnot001.discordbot
 
 import io.github.defnot001.SimpleChatbridge
+import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.minecraft.ChatFormatting
@@ -16,14 +17,34 @@ class MessageReceivedListener(private val server: MinecraftServer) : ListenerAda
         if (event.channel.id != SimpleChatbridge.config.discordChannelID) return
         if (event.isWebhookMessage) return
 
+
+        if (event.message.hasAttachment) {
+            sendIngameNotificationAttachment(event.author.name, event.message)
+        }
+
         sendIngameMessage(event.author.name, event.message.contentDisplay)
     }
 
     private fun sendIngameMessage(author: String, content: String) {
         val message = formatChatMessage(author, content)
 
+        if (content.isNotEmpty()) {
+            server.execute {
+                server.playerList.broadcastMessage(message, ChatType.CHAT, Util.NIL_UUID)
+            }
+        }
+    }
+
+    private fun sendIngameNotificationAttachment(author: String, message: Message) {
+        val attachmentMessage = if (message.hasMultipleAttachments) "multiple attachments" else "an attachment"
+
         server.execute {
-            server.playerList.broadcastMessage(message, ChatType.CHAT, Util.NIL_UUID)
+            server.playerList.broadcastMessage(
+                TextComponent("$author posted $attachmentMessage in Discord.")
+                    .withStyle(ChatFormatting.GRAY),
+                ChatType.CHAT,
+                Util.NIL_UUID
+            )
         }
     }
 
@@ -32,4 +53,8 @@ class MessageReceivedListener(private val server: MinecraftServer) : ListenerAda
                 .append(TextComponent(author).withStyle { it.withColor(ChatFormatting.GRAY) })
                 .append(TextComponent("] $content"))
     }
+
+    private val Message.hasAttachment get() = this.attachments.isNotEmpty()
+
+    private val Message.hasMultipleAttachments get() = this.attachments.size > 1
 }
