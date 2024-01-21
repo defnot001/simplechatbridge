6b0b7dfb800290b7461385a383d84ada954e023c
defnot001 <marc.miller994@gmail.com>
2024-01-21 19:46:52 +0100

    upgraded to version 1.20.4

--- src/main/java/io/github/defnot001/mixin/MixinPlayerAdvancements.java
+++ src/main/java/io/github/defnot001/mixin/MixinPlayerAdvancements.java
@@ -1,10 +1,8 @@
 package io.github.defnot001.mixin;
 
 import io.github.defnot001.minecraft.GameMessageHandler;
-import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.DisplayInfo;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.PlayerAdvancements;
 import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +10,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerAdvancements.class)
 public class MixinPlayerAdvancements {
