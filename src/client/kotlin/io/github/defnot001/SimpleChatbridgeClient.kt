035696e217271bbbad06e40f25e9b4e52aaad37c
defnot001 <marc.miller994@gmail.com>
2024-01-21 18:57:44 +0100

    delete client directory

--- src/client/kotlin/io/github/defnot001/SimpleChatbridgeClient.kt
+++
@@ -1,9 +0,0 @@
-package io.github.defnot001
-
-import net.fabricmc.api.ClientModInitializer
-
-object SimpleChatbridgeClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
