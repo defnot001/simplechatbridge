909615201d3561f5ef8844fa893e185b55d16182
defnot001 <defnot001@gmail>
2024-09-21 20:02:59 +0200

    fixed a critical bug that crashes servers

--- src/main/java/io/github/defnot001/mixin/MixinServerGamePacketListenerImpl.java
+++ src/main/java/io/github/defnot001/mixin/MixinServerGamePacketListenerImpl.java
@@ -1,7 +1,7 @@
 package io.github.defnot001.mixin;
 
 import io.github.defnot001.minecraft.GameMessageHandler;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.DisconnectionDetails;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +17,7 @@ public class MixinServerGamePacketListenerImpl {
     public ServerPlayer player;
 
     @Inject(method = "onDisconnect", at = @At("HEAD"))
-    private void onDisconnect(Component component, CallbackInfo ci) {
+    private void onDisconnect(DisconnectionDetails disconnectionDetails, CallbackInfo ci) {
         GameMessageHandler.INSTANCE.postSystemMessageToDiscord(this.player.getScoreboardName() + " left the game.");
     }
 }
